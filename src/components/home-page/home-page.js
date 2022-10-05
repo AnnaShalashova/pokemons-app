acd391b847286bbc8516ddb087d428dcc717a6f7
AnnaShalashova <anyashalashovaa@gmail.com>
2022-10-05 16:44:20 +0600

    Optimised loading image

--- src/components/home-page/home-page.js
+++ src/components/home-page/home-page.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./home-page.css"
+import "./home-page.css";
+import homeImg from "../images/Pokemon-PNG-HD.png";
 
 const HomePage = () => {
 
@@ -8,7 +9,7 @@ const HomePage = () => {
             <p className="home-header"> Welcome to Pokemon Home!</p>
             <p className="home-body">There is website where you can see all pokemons. Just click on "Pokemons" and you can choose any pokemon!</p>
             <img className="home-img" alt="img-home" width="200px" 
-                src="https://www.pngall.com/wp-content/uploads/2016/06/Pokemon-PNG-HD.png">
+                src={homeImg}>
             </img>
         </div>
     )
